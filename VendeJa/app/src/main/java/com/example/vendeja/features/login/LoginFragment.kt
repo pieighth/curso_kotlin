2253f52acc6cac82f47bf4917bc53d39fc0e7c94
Pedro Silveira <peagah@gmail.com>
2019-10-31 12:27:55 -0400

    Adicionado o projeto vendeJa

---
+++ VendeJa/app/src/main/java/com/example/vendeja/features/login/LoginFragment.kt
@@ -0,0 +1,37 @@
+package com.example.vendeja.features.login
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+import com.example.vendeja.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        btn_login.setOnClickListener {
+            navController.navigate(R.id.home_graph)
+        }
+    }
+}
